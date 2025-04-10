42493684162ce456ab8567d28f3ae81f4be69625
av-justin <justin@avalor.ai>
2025-04-10 13:33:43 +0200

    updated msg description and field name

--- build/parser.rs
+++ build/parser.rs
@@ -34,12 +34,12 @@ fn inject_custom_mav_cmds(profile: &mut MavProfile) {
         mav_cmd.entries.push(MavEnumEntry {
             value: Some(43003),
             name: "MAV_CMD_EXTERNAL_POSITION_ESTIMATE".to_string(),
-            description: Some("Send corrections for the global position estimate".to_string()),
+            description: Some("Provide an external position estimate for use when dead-reckoning. This is meant to be used for occasional position resets that may be provided by a external system such as a remote pilot using landmarks over a video link.".to_string()),
             params: Some(vec![
                 "transmission_time".to_string(),
                 "processing_time".to_string(),
                 "accuracy".to_string(),
-                "empty".to_string(),
+                "param4".to_string(),
                 "latitude".to_string(),
                 "longitude".to_string(),
                 "altitude".to_string(),
